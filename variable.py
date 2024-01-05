862ccf6ba257abdc42f6a760a6e7295674bbc73b
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-01-05 20:37:58 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,34 @@
+# a= 'vidhi'
+# A='4'
+# print(a)
+# print(A)
+
+#CAMEL CASE
+
+# vidhiIsNo='vidhi'
+
+#PASCAL CASE
+
+#MyVariableName='vidhi'
+
+#SNAKE CASE
+
+#my_variable_name='vidhi'
+
+# x=y=z='apple'
+
+# print(x)
+# print(y)
+# print(z)
+
+# fruits=["apple","orange","banana"]
+# x,y,z=fruits
+# print(x)
+# print(y)
+# print(z)
+
+x="awesome"
+
+def myfun():
+    print("python is" + x)
+myfun()
